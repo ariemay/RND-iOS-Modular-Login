4bf4586b9a0a15d26ca782866a75432e32127906
Arie May Wibowo <arie.wibowo@cimbniaga.co.id>
2021-03-13 03:43:56 +0700

    Initial commit

---
+++ rnd-login-module/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LoginViewController.swift
+//  rnd-login-module
+//
+//  Created by Arie May Wibowo on 12/03/21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+    
+    @IBAction func toMainPage(_ sender: Any, completion: @escaping (String) -> Void) {
+        completion("hajar")
+    }
+
+
+}
