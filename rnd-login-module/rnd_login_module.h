6a9ddd9133a7a59011827da21f2c4cc93333d2f9
Arie May Wibowo <arie.wibowo@cimbniaga.co.id>
2021-03-12 20:18:41 +0700

    Initial Commit

---
+++ rnd-login-module/rnd_login_module.h
@@ -0,0 +1,18 @@
+//
+//  rnd_login_module.h
+//  rnd-login-module
+//
+//  Created by Arie May Wibowo on 12/03/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for rnd_login_module.
+FOUNDATION_EXPORT double rnd_login_moduleVersionNumber;
+
+//! Project version string for rnd_login_module.
+FOUNDATION_EXPORT const unsigned char rnd_login_moduleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <rnd_login_module/PublicHeader.h>
+
+
